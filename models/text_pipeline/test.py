b8eefe5f77a6fb2ecd889848a31b66cf87516bdc
sameeksha327 <sameekshasathuri@gmail.com>
2026-02-16 19:40:00 +0530

    Implement text model evaluation in test.py
    Added code to load model and evaluate accuracy on text data.

--- models/text_pipeline/test.py
+++ models/text_pipeline/test.py
@@ -1,1 +1,15 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.metrics import accuracy_score
+
+model = load_model("text_model.h5")
+
+X = np.load("text_features.npy")
+y = np.load("text_labels.npy")
+
+pred = model.predict(X)
+y_pred = pred.argmax(axis=1)
+
+accuracy = accuracy_score(y, y_pred)
+print("Text Accuracy:", accuracy)
 
