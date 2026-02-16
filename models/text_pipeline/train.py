f07b45b1c1b1a76a5501806b8cf7925a6c68c131
sameeksha327 <sameekshasathuri@gmail.com>
2026-02-16 19:39:40 +0530

    Add LSTM model for text classification training
    Implemented a text classification model using LSTM.

--- models/text_pipeline/train.py
+++ models/text_pipeline/train.py
@@ -1,1 +1,30 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense
+from tensorflow.keras.utils import to_categorical
+
+# Load data
+X = np.load("text_features.npy")
+y = np.load("text_labels.npy")
+
+y = to_categorical(y)
+
+# Model
+model = Sequential()
+model.add(Embedding(input_dim=5000, output_dim=128, input_length=X.shape[1]))
+model.add(LSTM(64))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(y.shape[1], activation='softmax'))
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+model.fit(X, y, epochs=10, batch_size=32)
+
+model.save("text_model.h5")
+
+print("Text model trained")
 
