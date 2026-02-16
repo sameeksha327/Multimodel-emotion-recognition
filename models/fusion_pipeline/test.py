9a4a179e676b37e746bdeedeee46ead943983392
sameeksha327 <sameekshasathuri@gmail.com>
2026-02-16 19:40:58 +0530

    Add model evaluation code in test.py
    Load model and evaluate accuracy on predictions.

--- models/fusion_pipeline/test.py
+++ models/fusion_pipeline/test.py
@@ -1,1 +1,16 @@
+import numpy as np
+from tensorflow.keras.models import load_model
+from sklearn.metrics import accuracy_score
+
+model = load_model("fusion_model.h5")
+
+speech = np.load("speech_features.npy")
+text = np.load("text_features.npy")
+labels = np.load("labels.npy")
+
+pred = model.predict([speech, text])
+y_pred = pred.argmax(axis=1)
+
+accuracy = accuracy_score(labels, y_pred)
+print("Fusion Accuracy:", accuracy)
 
