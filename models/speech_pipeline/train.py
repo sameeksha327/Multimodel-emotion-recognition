78f78f512b8305b263d886c26a6e02cfe66c7d58
sameeksha327 <sameekshasathuri@gmail.com>
2026-02-16 19:39:05 +0530

    Update train.py

--- models/speech_pipeline/train.py
+++ models/speech_pipeline/train.py
@@ -1,1 +1,37 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
+# Load data
+X = np.load("speech_features.npy")
+y = np.load("speech_labels.npy")
+
+y = to_categorical(y)
+
+# Split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Model
+model = Sequential()
+model.add(Dense(128, activation='relu', input_shape=(X.shape[1],)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(y.shape[1], activation='softmax'))
+
+model.compile(
+    optimizer='adam',
+    loss='categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train
+model.fit(X_train, y_train, epochs=20, batch_size=32)
+
+# Save model
+model.save("speech_model.h5")
+
+print("Speech model trained")
 
